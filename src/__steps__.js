0b6c944e1f12ede447e0471d6a78c72a473c883f
SharikaJahanAunto <Sharikajahannn@gmail.com>
2023-04-22 16:01:22 +0600

    added handle

--- src/__steps__.js
+++ src/__steps__.js
@@ -6,3 +6,11 @@
  * 5. Build > Authentication > get started > enable sign in method
  * 6. create sign up and Login route
  * */ 
+
+/**
+ * 1. Create a context provider file
+ * 2. Create a context and set provider
+ * 3. Set the children props
+ * 4. set context value
+ * 5. set the provider
+ * */ 
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 import './SignUp.css'
 
 const SignUp = () => {
+    const [error, setError] = useState('')
+    const {createUser} = useContext(AuthContext)
+
+    const handleSignUp = event => {
+        event.preventDefault()
+
+        const form = event.target
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+        console.log(email, password, confirm);
+
+        setError('')
+        if(password !== confirm){
+            setError('Your Password did not match')
+            return
+        }
+        else if(password.length < 6){
+            setError('password must be 6 characters or longer')
+        }
+
+        createUser(email, password)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser); 
+        })
+        .catch(error =>{
+            console.log(error);
+            setError(error.message)
+        
+        })
+    }
+
     return (
         <div className='form-container'>
             <h2 className='form-title'>Sign Up</h2>
-            <form>
+            <form onSubmit={handleSignUp}>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" id="" required />
@@ -21,7 +56,8 @@ const SignUp = () => {
                 </div>
                 <input className='btn-submit' type="submit" value="Sign Up" />
             </form>
-            <p><small>Already have an account?? <Link to='login'>Login</Link></small></p>
+            <p><small>Already have an account?? <Link to='/login'>Login</Link></small></p>
+            <p className='text-error'>{error}</p>
         </div>
     );
 };
